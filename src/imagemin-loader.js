01f612761d514008225c87877cf3df5a0d930d3d
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2018-04-03 16:30:41 +0300

    refactor: better determination `bail` in `loader`

--- src/imagemin-loader.js
+++ src/imagemin-loader.js
@@ -7,15 +7,13 @@ const minify = require("./minify/minify");
 module.exports = function(content) {
   const options = loaderUtils.getOptions(this) || {};
   const callback = this.async();
+  /* eslint-disable no-underscore-dangle */
+  const bail =
+    (this._compiler && this._compiler.options && this._compiler.options.bail) ||
+    false;
 
-  if (typeof options.bail === "undefined") {
-    /* eslint-disable no-underscore-dangle */
-    options.bail =
-      (this._compiler &&
-        this._compiler.options &&
-        this._compiler.options.bail) ||
-      false;
-    /* eslint-enable no-underscore-dangle */
+  if (typeof options.bail !== "boolean" && bail) {
+    options.bail = bail;
   }
 
   return nodeify(minify(content, options), callback);
