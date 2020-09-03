97597a440156719c95985fb8c59e7475acfe3668
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2020-09-03 19:34:31 +0300

    test: refactor (#152)

---
+++ test/plugin-loader-option.test.js
@@ -0,0 +1,34 @@
+import { isOptimized, plugins, webpack } from './helpers';
+
+describe('plugin loader option', () => {
+  it('should optimizes all images (plugin standalone)', async () => {
+    const stats = await webpack({
+      emitPlugin: true,
+      imageminPluginOptions: {
+        minimizerOptions: { plugins },
+        loader: false,
+      },
+    });
+    const { compilation } = stats;
+    const { warnings, errors } = compilation;
+
+    expect(warnings).toHaveLength(0);
+    expect(errors).toHaveLength(0);
+
+    await expect(isOptimized('loader-test.gif', compilation)).resolves.toBe(
+      true
+    );
+    await expect(isOptimized('loader-test.jpg', compilation)).resolves.toBe(
+      true
+    );
+    await expect(isOptimized('loader-test.png', compilation)).resolves.toBe(
+      true
+    );
+    await expect(isOptimized('loader-test.svg', compilation)).resolves.toBe(
+      true
+    );
+    await expect(isOptimized('plugin-test.jpg', compilation)).resolves.toBe(
+      true
+    );
+  });
+});
